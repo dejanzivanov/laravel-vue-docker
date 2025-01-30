ed10e41207336644bfe014834306d4731216d95e
Dejan Zivanov <punisher0504@gmail.com>
2025-01-30 20:26:45 +0100

    Fix #11 - Fixed so frontend project is generated in proper directory

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,27 +1,15 @@
 #!/usr/bin/env sh
 set -e
 
-APP_DIR="/app"
+APP_DIR="/"
 
-echo "1. Current directory: $(pwd)"
+if [ -z "$(ls -A "$APP_DIR/frontend" 2>/dev/null)" ]; then
 
-if [ -z "$(ls -A "$APP_DIR" 2>/dev/null)" ]; then
-  echo "==> Frontend folder is empty. Creating new Vue app..."
-  
-  # Create Vue project with defaults in /app directory
   apk add --update nodejs npm
 
-  echo "2. Generating Vue project..."
   npm create vue@latest "frontend" -- --default
-  ls -lah
-  pwd
-  echo "3.432432423432432. Generating Vue project..."
 
-
-  # Move into app directory and install dependencies
-  cd "$APP_DIR"/frontend
-  pwd
-  echo "3. Installing dependencies in: $(pwd)"
+  cd "$APP_DIR"frontend
   npm install
 
 else
